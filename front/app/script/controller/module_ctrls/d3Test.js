ed021f5468ba37b7c8e4ae503764492101cd5f17
breakhappy <break_happy@163.com>
2015-01-05 16:19:41 +0800

    update

--- front/app/script/controller/module_ctrls/d3Test.js
+++ front/app/script/controller/module_ctrls/d3Test.js
@@ -7,7 +7,9 @@ define(function(require){
         var d3=require("D3")
 
         var viewModel= d3.select(element[0]).selectAll("p").data([1, 2, 3, 4, 5, 6]).enter()
-            viewModel.append("p") .style("border","1px solid red").text(function(d) { return "I’m number " + d + "!"; });
+        var pViews= viewModel.append("p").style("border","1px solid red").text(function(d) { return "I’m number " + d + "!"; });
+
+            pViews.append("span").style("margin","0px 50px").text("           this is the test")
 
     }]
 
